e742f39ded336166735a8db3d8f36182d414e538
tom <tpfitz42@gmail.com>
2025-04-25 13:18:34 -0600

    cors fix

--- next.config.js
+++ next.config.js
@@ -4,11 +4,32 @@ const nextConfig = {
   images: {
     unoptimized: true,
     domains: ['firebasestorage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/**',
+      },
+    ],
   },
   ...(process.env.NODE_ENV === 'production' ? {
     basePath: '/fhi-v4',
     assetPrefix: '/fhi-v4/',
   } : {}),
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getStorage, connectStorageEmulator } from 'firebase/storage';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
